32da7b487e25a0eb744e133275deb35a97b6e3ef
dittojoseph-urolime <78897746+dittojoseph-urolime@users.noreply.github.com>
2021-10-29 13:00:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Ditto! <br>";
+echo "Hello, World from Jenkins code deploy! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
