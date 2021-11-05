f14b856a3788fd778739744dbc7494186d8838e0
dittojoseph-urolime <78897746+dittojoseph-urolime@users.noreply.github.com>
2021-11-05 20:01:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World..........demo <br>";
+echo "Hello, World..........from ec2 <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
