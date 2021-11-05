ba3dd50b4886f762d15d40c9a92020da4cfd9fdb
dittojoseph-urolime <78897746+dittojoseph-urolime@users.noreply.github.com>
2021-11-05 19:42:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Partial Automation!!!!!!!!!!!!!!!!!! <br>";
+echo "Hello, World..........demo <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
